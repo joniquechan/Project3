d5475f4b9ce43676b11b42b1680b2034c5c54cbd
joniquechan <uniquejoniqueC@gmail.com>
2024-10-27 01:11:07 -0400

    some changes - not entirely fixed tho

--- src/main/java/com/mycompany/project3algo/Project3Algo.java
+++ src/main/java/com/mycompany/project3algo/Project3Algo.java
@@ -11,13 +11,10 @@ import java.util.Stack;
  * @author User
  */
 
- // !! todo:
- // - parse input
- // - unionfind 
- // - objects
 
 public class Project3Algo {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        System.out.println("enter input: ");
         Scanner sc = new Scanner(System.in);
 
         // problem instances
@@ -36,7 +33,6 @@ public class Project3Algo {
             Dominion[][][] galaxy = new Dominion[n][m][k]; 
 
             // store monarchies as objects and process in reverse order to make them grow
-            // should we use a stack or a list
             Stack<Monarchy> monarchies = new Stack<>();
 
             for (int j = 0; j < l; j++) {
@@ -45,7 +41,6 @@ public class Project3Algo {
 
                 for (int x = 0; x < numDominions; x++) {
                     dominions[x] = sc.nextInt();
-                    //Set.makeSet(dominions[x]);
                 }
 
                 monarchies.push(new Monarchy(dominions));
@@ -53,50 +48,95 @@ public class Project3Algo {
             
             int disconnectedMonths = 0;
             int numSets = 0;
-            while(!monarchies.empty()){
+
+            while (!monarchies.isEmpty()) {
                 Monarchy mon = monarchies.pop();
                 int[] dominions = mon.getDominions();
 
                 Dominion[] doms = new Dominion[dominions.length];
-                for(int j = 0; j < dominions.length; j++){
+                for (int j = 0; j < dominions.length; j++) {
                     int dom = dominions[j];
                     int kk = dom / (n*m);
-                    int mm = (dom % n*m) / n;
-                    int nn = (dom % n*m) % n;
+                    int mm = (dom % (n*m)) / n;
+                    int nn = (dom % (n*m)) % n;
 
                     Dominion d = new Dominion(nn, mm, kk);
                     galaxy[nn][mm][kk] = d;
                     doms[j] = d;
                     Set.makeSet(galaxy[nn][mm][kk]);
+                    numSets++;
                 }
 
                 for(int j = 1; j < doms.length; j++){
                     Set.union(doms[0], doms[j]);
+                    numSets--;
                 }
-                numSets++;
+                
+                //numSets++;
                 
                 //check neighbors and connect if necessary
                 for(int j = 1; j < doms.length; j++){
                     Dominion dom = doms[j];
+                    
                     //check left
-                    if(dom.n != 0){
+                    if(dom.n > 0){
                         Dominion left = galaxy[dom.n - 1][dom.m][dom.k];
-                        if(left != null){
-                            if(Set.findSet(left) != Set.findSet(dom)){
-                                Set.union(dom, left);
-                                numSets --;
-                            }
+                        if(left != null && Set.findSet(left) != Set.findSet(dom)){
+                            Set.union(dom, left);
+                            numSets--;
                         }
                     }
+
                     //check right
+                    if (dom.n < n - 1) {
+                        Dominion right = galaxy[dom.n + 1][dom.m][dom.k];
+                        if (right != null && Set.findSet(right) != Set.findSet(dom)) {
+                            Set.union(dom, right);
+                            numSets--;
+                        }
+                    }
+
                     //check front
+                    if (dom.m > 0) {
+                        Dominion front = galaxy[dom.n][dom.m - 1][dom.k];
+                        if (front != null && Set.findSet(front) != Set.findSet(dom)) {
+                            Set.union(dom, front);
+                            numSets--;
+                        }
+                    }
                     //check back
+                    if (dom.m < m - 1) {
+                        Dominion back = galaxy[dom.n][dom.m + 1][dom.k];
+                        if (back != null && Set.findSet(back) != Set.findSet(dom)) {
+                            Set.union(dom, back);
+                            numSets--;
+                        }
+                    }
                     //check up
+                    if (dom.k > 0) {
+                        Dominion up = galaxy[dom.n][dom.m][dom.k - 1];
+                        if (up != null && Set.findSet(up) != Set.findSet(dom)) {
+                            Set.union(dom, up);
+                            numSets--;
+                        }
+                    }
                     //check down
+                    if (dom.k < k - 1) {
+                        Dominion down = galaxy[dom.n][dom.m][dom.k + 1];
+                        if (down != null && Set.findSet(down) != Set.findSet(dom)) {
+                            Set.union(dom, down);
+                            numSets--;
+                        }
+                    }
                 }
 
             }
             // check numSets == 1 and update months
+            if (numSets > 1) {
+                disconnectedMonths++;
+            }
+
+            System.out.println("disconnected months: " + disconnectedMonths);
         }
 
         sc.close();
--- src/main/java/com/mycompany/project3algo/Set.java
+++ src/main/java/com/mycompany/project3algo/Set.java
@@ -1,47 +1,33 @@
 package com.mycompany.project3algo;
 
 public class Set {
-    
-    private int[] parent;
-    private int[] size;
-    private int[] rank;
-    private int sets;
-
-    // constructor for initializing array?
-    public Set(int dominions) {
-
-    }
 
     public static void union(Dominion x, Dominion y) {
-        // pick new representative from one
-        // connect two sets by rank
-        // sets--
         Dominion xparent = findSet(x);
         Dominion yparent = findSet(y);
         int xrank = xparent.getRank();
         int yrank = yparent.getRank();
 
-        if(xparent.getRank() >= yparent.getRank()){
-            yparent.setParent(xparent);
-            if(xrank == yrank){
-                xparent.setRank(xrank + 1);
-            }
-
+        // same parent
+        if (xparent == yparent) {
+            return;
         }
-        else{
+        // different parent, check rank
+        if (xrank > yrank) {
+            yparent.setParent(xparent);
+        }
+        else if (xrank < yrank) {
             xparent.setParent(yparent);
-            if(xrank == yrank){
-                yparent.setRank(yrank + 1);
-            }
+        } 
+        else {
+            yparent.setParent(xparent);
+            xparent.setRank(xrank + 1);
         }
     }
 
     public static void makeSet(Dominion dominion) {
-        // create set for dominion
-        // check if alr in a set -> check neighbors (helper function??)
-        // sets++
-        // return set
         dominion.setParent(dominion);
+        dominion.setRank(0);
     }
 
     public static Dominion findSet(Dominion d) {
@@ -52,20 +38,4 @@ public class Set {
         }
         return d.getParent();
     }
-
-    // remove dominion from empire
-    public void remove(int dominion) {
-
-    }
-
-    public boolean isConnected() {
-        return sets == 1;
-    }
-    // representative
-    // makeArray
-    // makeSet <- sets +1
-    // findSet
-    // checkneighbors 
-    // bool isConnected
-    // if not connected months++
 }
